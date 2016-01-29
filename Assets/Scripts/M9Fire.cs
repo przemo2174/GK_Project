fffd0b6909ff75e7b5f4e459b3122dc5c9608703
przemo2174 <przemo217@hotmail.com>
2016-01-29 20:18:23 +0100

    added shoot animation

---
+++ Assets/Scripts/M9Fire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class M9Fire : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            AudioSource gunSound = GetComponent<AudioSource>();
+            gunSound.Play();
+            GetComponent<Animation>().Play("M9Shoot");
+        }
+	}
+}
